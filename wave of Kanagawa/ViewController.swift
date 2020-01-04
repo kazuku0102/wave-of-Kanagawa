1bcce6de41afc84aeffdd023ab3b1509b1ffaac4
Shelly <shelly980102@gmail.com>
2020-01-04 14:05:29 +0800

    Initial Commit

---
+++ wave of Kanagawa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wave of Kanagawa
+//
+//  Created by 蕭聿莘 on 2020/1/4.
+//  Copyright © 2020 Kazuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
